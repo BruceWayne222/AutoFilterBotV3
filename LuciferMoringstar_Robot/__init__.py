5df6aac76ede33e27acb021b947a25a68e5b8a07
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2021-12-21 10:21:20 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,31 +11,31 @@ from .Channel import (
 )
 
 HELP = """
-Basic Commads
+Basic Commands
 
-/start :- check am Alive
+➻/start : check am Alive
 
 Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+➻/broadcast : Replay Any Message or Media
+➻/stats : user Status
+➻/ban_user : click ban_user more info
+➻/unban_user : click unban_user more info
+➻/banned_users : Banned User Details
+➻/total : How Many Files Added In Database
+➻/logger : Get Logs
+➻/delete : Delete File From Database
 
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➻ Name :  Movie Hub SL Filter 🎬
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➻ Creator : [Jason Todd](
+https://t.me/MHSLsupport)
 
-➥ Language : Python3
+➻ Language : Python3
 
-➥ Library : Pyrogram Asyncio 1.13.0
+➻ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+
 """
