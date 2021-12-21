0aac33a8db01f3b101bfcda4a34ea96d67b1c1fb
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2021-12-21 10:29:17 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -117,8 +117,7 @@ async def start(bot, message):
                 [[
                 InlineKeyboardButton("Search Here", switch_inline_query_current_chat='')
                 ],[
-                InlineKeyboardButton("🆘 Help
-                                     ", callback_data="help"),
+                InlineKeyboardButton("🆘 Help", callback_data="help"),
                 InlineKeyboardButton("About", callback_data="about")
                 ]]
             )
